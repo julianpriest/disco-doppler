f36a61fdcb5dff57f9ac8a63bca84838f819e831
Julian Priest <julian@greenbench.org>
2026-03-26 01:09:37 +0100

    add trigger script

--- dopplerd.sh
+++ dopplerd.sh
@@ -3,21 +3,40 @@
 # trigger the doppler.py script at an interval in seconds
 # copy the result to input.csh for use by csh
 # -q flag stops debug output of doppler.py
-# uncomment line 21 for logging and montior with tail -f doppler.log
 
-if [ -z "$1" ];
-then
-    my_delay=0.7
-else
-    my_delay=$1
-fi
+usage()
+{
+    echo "Usage: $0 [-l] log [-d] delay [-h] help"
+    echo "[ctrl-c] quit, tail -f doppler.log to monitor"
+    echo "frequencies output to input.csh every [delay] s"
+    exit 1
+}
+
+# defaults
+my_log=0
+my_delay=0.7
+
+while getopts "ld:h" opt;
+do
+    case $opt in
+	l) my_log=1; echo "Logging enabled"; echo $my_log;;
+	d) my_delay=$OPTARG; echo "Delay: $my_delay";;
+	h) usage;;
+	:) echo "#Option -$OPTARG requires an argument." >&2;;
+    esac
+done
+
 echo "Starting dopplerd with $my_delay s interval"
+
 while true;
 do
     my_time="$(date -uIs)"
     ./doppler.py 55.6167 12.65 -t $my_time -o tmp.csh -d 5 -q
     mv -f tmp.csh input.csh
-    echo $my_time `cat input.csh` >> doppler.log
+    if [ $my_log -eq 1 ];
+    then
+       echo $my_time `cat input.csh` >> doppler.log
+    fi
     sleep $my_delay
 done
 
